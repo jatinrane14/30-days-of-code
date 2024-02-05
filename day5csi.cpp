3a034674d9bb60d5101d88a4831a032569ef0199
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2024-02-05 18:45:36 +0530

    Update day5csi.cpp

--- day5csi.cpp
+++ day5csi.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int a = 0, b = 1, n=10, c;
-    cout << "Fibonachi series upto" << n << "terms" << endl;
+    cout << "Fibonachi series upto " << n << " terms" << endl;
     cout << a <<endl<< b <<endl;
     for (int i = 0; i <(n-2); i++)
     {
