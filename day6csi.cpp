45075425c8a43854fc9895589e1b6ab9419a9f64
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2024-02-06 18:44:00 +0530

    Add files via upload

---
+++ day6csi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    int num_list[n];    // let n = 4
+    cout<<"Enter number of terms: ";
+    cin >> n;
+    for (int i=0; i<n; i++)
+    {
+        cout << "Enter number" <<i+1<<": ";
+        cin >> num_list[i];  // user input the vlaues eg: 11, 45, 89, 45, 20
+    }
+    for (int i=0; i<n; i++)
+    {
+        cout << num_list[i]<< " ";
+    }
+    // output : 11 45 89 45 20
+    return 0;
+}
