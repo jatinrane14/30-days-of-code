71702227596a13f8027a1c24f827a98537c6e7cb
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2024-02-21 20:34:26 +0530

    Update day21csi.cpp

--- day21csi.cpp
+++ day21csi.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 int factorial(int num){
+    int fac;
     if (num < 0) {
         cerr << "The number cannot be negative";
         return -1; // Indicate error
     }
-    else if (num == 0 || num == 1) {
-        return 1; // Base case: factorial of 0 and 1 is 1
-    }
-    else {
-        return num * factorial(num - 1); // Recursive call for factorial
+    else{
+      if(num==0||num==1){
+             return 1;
+      }
+      else{
+         fac = num*factorial(num-1);
+         return fac;
+      }
     }
 }
 
